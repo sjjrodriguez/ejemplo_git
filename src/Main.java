2065979d57c6f3da1463caaa5ecb87262522c6b4
sjjrodriguez <sjrodriguez120@gmail.com>
2025-02-21 13:34:24 -0500

    reversa en git

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("ejemplo git");
         System.out.println("hola mundo");
+        System.out.println("reversa en git");
     }
 }
